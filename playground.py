5573a912feacfeb6725e660c1679be55578d3190
Yifei Liu <34564223+EzekielDaun@users.noreply.github.com>
2025-12-18 12:03:16 -0800

    SE3SO22 Stewart WIP

---
+++ playground.py
@@ -0,0 +1,29 @@
+import jax.numpy as jnp
+from jaxlie import SE3, SO2, SO3
+
+from se3_rpkm.data_types import SE3SO22
+from se3_rpkm.se3so22.stewart.core import SE3SO22StewartKinematics
+
+if __name__ == "__main__":
+    beta = 2.25
+    rot_90 = SO3.from_z_radians(jnp.linspace(0, 2 * jnp.pi, 4, endpoint=False))
+    rot_180 = SO3.from_z_radians(jnp.linspace(0, 2 * jnp.pi, 2, endpoint=False))
+    dimension = SE3SO22StewartKinematics(
+        a_i=rot_90.inverse().apply(jnp.array([beta, beta, 0.0])),
+        a_j1=rot_180.apply(jnp.array([beta, beta, 0.0])),
+        a_j2=rot_180.apply(jnp.array([beta, -beta, 0.0])),
+        v_i=jnp.array(
+            [
+                [1.0, 0.0, 0.0],
+                [1.0, 0.0, 0.0],
+                [-1.0, 0.0, 0.0],
+                [-1.0, 0.0, 0.0],
+            ]
+        ),
+        v_j=rot_180.apply(jnp.array([1.0, 0.0, 0.0])),
+        l_j=jnp.array([0.1, 0.1]),
+    )
+    x = SE3SO22(
+        pose=SE3.identity(), rdof=SO2.from_radians(jnp.array([jnp.pi / 2, jnp.pi / 2]))
+    )
+    print(dimension.ik(x))
--- src/se3_rpkm/data_types.py
+++ src/se3_rpkm/data_types.py
@@ -1,24 +1,51 @@
-from typing import Annotated
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Annotated
 
 import jax_dataclasses as jdc
-from jaxlie import SE3, SO2
+from jaxlie import SE3, SO2, SO3
 from jaxtyping import Array, Float
 
 from .lie_group_kinematics import (
     AbstractLieGroupTree,
 )
 
+if TYPE_CHECKING:
+    import mujoco
+
+SO22 = Annotated[SO2, "Batch of 2 SO2"]
 SO23 = Annotated[SO2, "Batch of 3 SO2"]
 SO29 = Annotated[SO2, "Batch of 9 SO2"]
 Vec3 = Annotated[Float[Array, "3"], ""]
+Vec8 = Annotated[Float[Array, "8"], ""]
 Vec9 = Annotated[Float[Array, "9"], ""]
+Mat2x3 = Annotated[Float[Array, "2 3"], ""]  # noqa: F722
 Mat3x3 = Annotated[Float[Array, "3 3"], ""]  # noqa: F722
+Mat4x3 = Annotated[Float[Array, "4 3"], ""]  # noqa: F722
 Mat6x6 = Annotated[Float[Array, "6 6"], ""]  # noqa: F722
-Mat3x15 = Annotated[Float[Array, "3 15"], ""]  # noqa: F722
+Mat8x8 = Annotated[Float[Array, "8 8"], ""]  # noqa: F722
 Mat9x9 = Annotated[Float[Array, "9 9"], ""]  # noqa: F722
+Mat3x15 = Annotated[Float[Array, "3 15"], ""]  # noqa: F722
+
+
+@jdc.pytree_dataclass(frozen=True, slots=True)
+class SE3SO22(AbstractLieGroupTree):
+    pose: SE3
+    rdof: SO22
 
 
 @jdc.pytree_dataclass(frozen=True, slots=True)
 class SE3SO23(AbstractLieGroupTree):
     pose: SE3
     rdof: SO23
+
+
+@jdc.pytree_dataclass(frozen=True, slots=True)
+class SE3SO3(AbstractLieGroupTree):
+    pose: SE3
+    rdof: SO3
+
+
+@jdc.pytree_dataclass(frozen=True, slots=True)
+class SE3R3(AbstractLieGroupTree):
+    pose: SE3
+    rdof: Vec3
